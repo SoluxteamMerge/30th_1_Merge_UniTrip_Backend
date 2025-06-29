f335b909a234e8c99b5d48696a6b37c10868bbb7
eunseo0903 <alice5808@naver.com>
2025-06-29 22:40:20 +0900

    initial commit

---
+++ src/main/java/com/Solux/UniTrip/UniTripApplication.java
@@ -0,0 +1,13 @@
+package com.Solux.UniTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Solux/UniTrip/UniTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Solux.UniTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
