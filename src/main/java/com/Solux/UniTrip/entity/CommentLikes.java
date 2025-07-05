06f6b3b58e5264d641701b921fd11eaeee0f69d0
eunseo0903 <alice5808@naver.com>
2025-07-05 15:02:43 +0900

    Add CommentLikes entity

---
+++ src/main/java/com/Solux/UniTrip/entity/CommentLikes.java
@@ -0,0 +1,34 @@
+package com.Solux.UniTrip.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment_likes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentLikes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    private Comment comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime likedAt;
+
+    @Column(nullable = false)
+    private boolean status;
+}
+
