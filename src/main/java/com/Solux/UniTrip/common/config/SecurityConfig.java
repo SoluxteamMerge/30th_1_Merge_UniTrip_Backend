f87f856ff5bb70630070f39d16d40c46e570d617
eunseo0903 <alice5808@naver.com>
2025-07-07 13:33:42 +0900

    [fix] add OAuth2 in Security Config

--- src/main/java/com/Solux/UniTrip/common/config/SecurityConfig.java
+++ src/main/java/com/Solux/UniTrip/common/config/SecurityConfig.java
@@ -1,26 +1,37 @@
 package com.Solux.UniTrip.common.config;
 
+import com.Solux.UniTrip.common.apiPayload.hadler.OAuth2SuccessHandler;
+import com.Solux.UniTrip.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Configuration;
 
-@org.springframework.context.annotation.Configuration
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-
-                        .requestMatchers( "/", "/css/**", "/js/**", "/images/**",
-                                "/api/place/search").permitAll()
-
+                        .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/api/place/search").permitAll()
                         .requestMatchers("/api/comments/**").authenticated()
-
+                        .requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
                         .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(oAuth2SuccessHandler)
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
                 );
+
         return http.build();
     }
 }
-
