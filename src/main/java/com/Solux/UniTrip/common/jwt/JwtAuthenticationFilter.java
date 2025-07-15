f1bd0b363752af3365e40185eb7d2a29711932a4
eunseo0903 <alice5808@naver.com>
2025-07-15 15:02:29 +0900

    [fix] 인증 부분에서 이메일 대신 유저 객체 받아오도록 수정

--- src/main/java/com/Solux/UniTrip/common/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/Solux/UniTrip/common/jwt/JwtAuthenticationFilter.java
@@ -62,7 +62,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         UsernamePasswordAuthenticationToken authentication =
                 new UsernamePasswordAuthenticationToken(
-                        email,
+                        user,
                         null,
                         List.of(new SimpleGrantedAuthority("ROLE_USER")) // 권한 부여
                 );
