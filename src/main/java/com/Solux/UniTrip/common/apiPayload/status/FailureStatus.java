cb38fef06647f5e699b684a5c13cbb3bc6b8131c
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-07-23 00:23:24 +0900

    [Add] 예외 처리 코드 추가

--- src/main/java/com/Solux/UniTrip/common/apiPayload/status/FailureStatus.java
+++ src/main/java/com/Solux/UniTrip/common/apiPayload/status/FailureStatus.java
@@ -32,8 +32,13 @@ public enum FailureStatus implements Basecode {
     _PROFILEIMAGE_UPLOAD_FAILURE(500, "프로필 이미지 업로드에 실패하였습니다."),
     _PROFILEIMAGE_NOT_FOUND(400, "삭제할 프로필 사진이 없습니다."),
     SCHEDULE_OVERLAP(409, "해당 기간에 이미 등록된 일정이 존재합니다."),
-    RATING_INVALID(400, "별점은 0.5 단위로 입력되어야 합니다.");
-
+    RATING_INVALID(400, "별점은 0.5 단위로 입력되어야 합니다."),
+    _BOARDTYPE_NOT_NULL(400, "BoardType must not be empty"),
+    _INVAILD_BOARDTYPE(400, "Invalid BoardType"),
+    _CATEGORY_NOT_NULL(400, "Category name must not be empty"),
+    _OVERNIGHT_NOT_NULL(400,"overnightFlag must not be null for 모임구인 type"),
+    _RECRUITMENTCNT_NOT_NULL(400, "recruitmentCnt must not be null for 모임구인 type"),
+    _CATEGORY_NOT_FOUND(400, "카테고리를 찾을 수 없습니다.");
 
 
     private final int code;
