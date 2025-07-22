3e30d6e258765201fef04ee94cdf182d17b02e09
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-07-23 00:10:02 +0900

    [Fix] _BAD_REQUSET 오타 수정

--- src/main/java/com/Solux/UniTrip/common/apiPayload/status/FailureStatus.java
+++ src/main/java/com/Solux/UniTrip/common/apiPayload/status/FailureStatus.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 public enum FailureStatus implements Basecode {
 
     //enum 에서 상수는 필드 선언보다 위에 있어야 함.
-    _BAD_REQUSET(400, "잘못된 요청입니다."),
+    _BAD_REQUEST(400, "잘못된 요청입니다."),
     _USER_NOT_FOUND(400, "존재하지 않는 회원입니다."),
     _UNAUTHORIZED(401, "인증되지 않은 사용자입니다."),
     _INTERNAL_SERVER_ERROR(500, "서버 내부 오류가 발생했습니다."),
