12eecc2563ed1b46a4050840cd3c9135fe84da0f
eunseo0903 <alice5808@naver.com>
2025-07-24 22:44:58 +0900

    ForwardedHeaderConfig 수정

--- src/main/java/com/Solux/UniTrip/common/config/ForwardedHeaderConfig.java
+++ src/main/java/com/Solux/UniTrip/common/config/ForwardedHeaderConfig.java
@@ -1,5 +1,6 @@
 package com.Solux.UniTrip.common.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
@@ -8,7 +9,10 @@ import org.springframework.web.filter.ForwardedHeaderFilter;
 public class ForwardedHeaderConfig {
 
     @Bean
-    public ForwardedHeaderFilter forwardedHeaderFilter() {
-        return new ForwardedHeaderFilter();
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> filterRegBean = new FilterRegistrationBean<>();
+        filterRegBean.setFilter(new ForwardedHeaderFilter());
+        filterRegBean.setOrder(0); // ★ Security filter chain보다 앞에 오도록 설정
+        return filterRegBean;
     }
 }
