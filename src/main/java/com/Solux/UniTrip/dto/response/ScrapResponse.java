153c8bd1416e4ab0729f6b6a13fd818c5b0db913
eunseo0903 <alice5808@naver.com>
2025-07-31 00:26:55 +0900

    스크랩 response 수정

--- src/main/java/com/Solux/UniTrip/dto/response/ScrapResponse.java
+++ src/main/java/com/Solux/UniTrip/dto/response/ScrapResponse.java
@@ -1,32 +1,52 @@
 package com.Solux.UniTrip.dto.response;
 
 import com.Solux.UniTrip.entity.Board;
+import com.Solux.UniTrip.entity.Image;
+import com.Solux.UniTrip.entity.Rating;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 
 @Getter
 @Builder
 public class ScrapResponse {
 
-    private String thumbnailUrl;
+    private String imageUrl;
     private String nickname;
     private Long postId;
     private String postTitle;
-    private Integer commentCount;
-    private Integer likeCount;
-    private Integer scrapCount;
+    private Double rating;
+    private int likeCount;
+    private int scrapCount;
     private String categoryName;
 
-    public static ScrapResponse from(Board board) {
-        return ScrapResponse.builder()
-                //.thumbnailUrl(board.getThumbnailUrl())
+    public static ReviewResultResponse from(Board board) {
+        List<Image> images = board.getImages();
+        String imageUrl = (images != null && !images.isEmpty()) ? images.get(0).getImageUrl() : null;
+
+        // 평균 평점 계산
+        List<Rating> ratings = board.getRatings();
+        double averageRating = 0.0;
+        if (ratings != null && !ratings.isEmpty()) {
+            averageRating = ratings.stream()
+                    .mapToDouble(Rating::getRating)
+                    .average()
+                    .orElse(0.0);
+
+            // 소수점 첫째 자리로 반올림
+            averageRating = Math.round(averageRating * 10) / 10.0;
+        }
+
+        return ReviewResultResponse.builder()
+                .imageUrl(imageUrl)
                 .nickname(board.getUser().getNickname())
                 .postId(board.getPostId())
                 .postTitle(board.getTitle())
-                //.commentCount(board.getCommentList().size())
-                //.likeCount(board.getLikeCount())
-                //.scrapCount(board.getScrapCount())
+                .rating(averageRating)
+                .likeCount(board.getLikes())
+                .scrapCount(board.getScraps())
                 .categoryName(board.getCategory().getCategoryName())
                 .build();
     }
